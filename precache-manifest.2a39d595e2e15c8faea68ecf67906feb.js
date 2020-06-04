9a63e04b7b843c4b7d0fe9dfd999de1479e74bf4
Joaquin Fernandez <copako12@gmail.com>
2020-06-04 16:50:39 -0300

    Updates

---
+++ precache-manifest.2a39d595e2e15c8faea68ecf67906feb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68c3d6b7af655233884b399a5e92354",
+    "url": "/bank-u/index.html"
+  },
+  {
+    "revision": "868a6323d8a3ff6fcf6b",
+    "url": "/bank-u/static/css/main.f2f2870e.chunk.css"
+  },
+  {
+    "revision": "fd52860d6d8405226a04",
+    "url": "/bank-u/static/js/2.c9c1c1a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bank-u/static/js/2.c9c1c1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "868a6323d8a3ff6fcf6b",
+    "url": "/bank-u/static/js/main.c4cb45f0.chunk.js"
+  },
+  {
+    "revision": "b7efb1bf92cb6c9df214",
+    "url": "/bank-u/static/js/runtime-main.a2aed773.js"
+  }
+]);
